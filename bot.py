a696d6ffb597a23b3a8e7ee75abd8461cb8a93f0
Ivan Nikiforov <Travyanoi@users.noreply.github.com>
2023-07-06 11:46:01 +0900

    Added expiration time of subscription for json file in relation to choosing time of sub

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import json
 import datetime
 import subprocess
+import re
 
 import peewee
 from peewee import Model, CharField
@@ -13,6 +14,7 @@ from dotenv import load_dotenv
 
 db = peewee.SqliteDatabase("TG-VPN-Payment/UserInfo.sqlite")
 
+regex_for_digit = re.compile(r"(\d+)")
 
 class UserInfo(Model):
     chat_id = CharField(max_length=64, primary_key=True)
@@ -31,23 +33,29 @@ bot = Bot(token=os.environ.get("BOT_TOKEN"))
 dp = Dispatcher(bot)
 
 
-def json_formatter(chat_id: UserInfo.chat_id):
+def json_formatter(chat_id: UserInfo.chat_id, duration_of_sub: str):
     os.chdir("TG-VPN-Payment")
     data = json.load(open('data.json'))
 
     username: UserInfo = UserInfo.get(UserInfo.chat_id == chat_id)
 
+    print(duration_of_sub)
     subprocess.check_output(f"wg genkey | tee {username.first_name}_privatekey |"
                             f" wg pubkey | tee {username.first_name}_publickey",
                             shell=True).decode("utf-8").strip()
     
     with open(f"{username.first_name}_privatekey") as privkey:
         with open(f"{username.first_name}_publickey") as pubkey:
+            created_at_time = datetime.datetime.now()
+            expiration_date = created_at_time + datetime.timedelta(days=30 * int(duration_of_sub))
+            
             data["clients"].update({f"{username.first_name}": {"publickey": pubkey.read().strip(),
                                                     "privatekey": f"{privkey.read().strip()}",
-                                                    "created_at": f"{datetime.datetime.now()}",
+                                                    "created_at": f"{created_at_time}",
+                                                    "expiration_of_sub_date" : f"{expiration_date}",
                                                     "enable": True
                                                     }})
+            
             os.remove(f"{privkey.name}")
             os.remove(f"{pubkey.name}")
 
@@ -69,15 +77,16 @@ async def cmd_start(message: types.Message):
 
     await bot.send_message(chat_id=message.chat.id, reply_markup=types.InlineKeyboardMarkup(inline_keyboard=kb),
                            text="Привет! Через данного бота у вас есть возможность "
-                                "приобрести впн по довольно низкой цене!\nПожалуйста, выберите продолжительность "
+                                "приобрести VPN по низкой цене!\nПожалуйста, выберите продолжительность "
                                 "подписки!")
 
 
 @dp.callback_query_handler(text=["1_month_sub", "6_months_sub", "12_months_sub"])
 async def payment_cmd(message: types.CallbackQuery):
+    duration_of_subscription = re.split(regex_for_digit, message.data)
     await bot.send_message(chat_id=message.message.chat.id, text="Сейчас все сделаю...")
 
-    json_formatter(message.message.chat.id)
+    json_formatter(message.message.chat.id, str(duration_of_subscription[1]))
 
 
 async def main():
